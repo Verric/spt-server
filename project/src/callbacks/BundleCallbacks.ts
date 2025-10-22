8e6093ce48a96c29ac0a26b0bab9be7d71c409d9
verric <beijing-planes@hotmail.com>
2025-10-22 20:20:12 +1100

    modernise BundleCallback

--- project/src/callbacks/BundleCallbacks.ts
+++ project/src/callbacks/BundleCallbacks.ts
@@ -8,23 +8,29 @@ import { inject, injectable } from "tsyringe";
 @injectable()
 export class BundleCallbacks {
     protected httpConfig: IHttpConfig;
+    protected httpResponse: HttpResponseUtil;
+    protected bundleLoader: BundleLoader;
+    protected configServer: ConfigServer;
 
     constructor(
-        @inject("HttpResponseUtil") protected httpResponse: HttpResponseUtil,
-        @inject("BundleLoader") protected bundleLoader: BundleLoader,
-        @inject("ConfigServer") protected configServer: ConfigServer,
+        @inject("HttpResponseUtil") httpResponse: HttpResponseUtil,
+        @inject("BundleLoader") bundleLoader: BundleLoader,
+        @inject("ConfigServer") configServer: ConfigServer
     ) {
+        this.configServer = configServer;
         this.httpConfig = this.configServer.getConfig(ConfigTypes.HTTP);
+        this.httpResponse = httpResponse;
+        this.bundleLoader = bundleLoader;
     }
 
     /**
      * Handle singleplayer/bundles
      */
-    public getBundles(url: string, info: any, sessionID: string): string {
+    public getBundles(_url: string, _info: any, _sessionID: string): string {
         return this.httpResponse.noBody(this.bundleLoader.getBundles());
     }
 
-    public getBundle(url: string, info: any, sessionID: string): string {
+    public getBundle(_url: string, _info: any, _sessionID: string): string {
         return "BUNDLE";
     }
 }
